4759d32303b361a7c8d7e1f322fa1e3e9156d1db
Etelson Alcius <69489350+Sonnyywithanyextray@users.noreply.github.com>
2023-01-08 13:21:14 -0500

    Update 2.0
    The visualize_referral_count function includes the platform parameter to handle data in different formats that may be returned by the API for iOS and Android apps. When using this function, you must specify the platform ('ios' or 'android') and provide the appropriate API URL for referral count data. The function will then make a request to this URL to retrieve the data and generate a histogram plot.

--- Main.py
+++ Main.py
@@ -1,61 +1,111 @@
 import matplotlib.pyplot as plt
 import requests
 
-def visualize_download_sources(api_url):
+def visualize_download_sources(api_url, platform):
     # Make request to API to retrieve download data
     response = requests.get(api_url)
     download_data = response.json()
 
     # Process download data to create pie chart
-    labels = download_data.keys()
-    sizes = download_data.values()
+    if platform == 'ios':
+        # iOS data processing code goes here
+        labels = download_data.keys()
+        sizes = download_data.values()
+    elif platform == 'android':
+        # Android data processing code goes here
+        labels = download_data['sources'].keys()
+        sizes = download_data['sources'].values()
+    else:
+        raise ValueError('Invalid platform specified')
     fig1, ax1 = plt.subplots()
     ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
     plt.show()
 
-def visualize_daily_usage(api_url):
+def visualize_daily_usage(api_url, platform):
     # Make request to API to retrieve daily usage data
     response = requests.get(api_url)
     daily_usage = response.json()
 
+    # Process daily usage data
+    if platform == 'ios':
+        # iOS data processing code goes here
+        usage_data = daily_usage
+    elif platform == 'android':
+        # Android data processing code goes here
+        usage_data = daily_usage['usage']
+    else:
+        raise ValueError('Invalid platform specified')
+
     # Create line plot
-    plt.plot(daily_usage)
+    plt.plot(usage_data)
     plt.xlabel('Day')
     plt.ylabel('Usage (minutes)')
     plt.show()
 
-def visualize_weekly_usage(api_url):
+def visualize_weekly_usage(api_url, platform):
     # Make request to API to retrieve weekly usage data
     response = requests.get(api_url)
     weekly_usage = response.json()
 
+    # Process weekly usage data
+    if platform == 'ios':
+        # iOS data processing code goes here
+        usage_data = weekly_usage
+    elif platform == 'android':
+        # Android data processing code goes here
+        usage_data = weekly_usage['usage']
+    else:
+        raise ValueError('Invalid platform specified')
+
     # Create bar plot
-    plt.bar(range(len(weekly_usage)), weekly_usage.values(), align='center')
-    plt.xticks(range(len(weekly_usage)), list(weekly_usage.keys()))
+    plt.bar(range(len(usage_data)), usage_data.values(), align='center')
+    plt.xticks(range(len(usage_data)), list(usage_data.keys()))
     plt.xlabel('Week')
     plt.ylabel('Usage (minutes)')
     plt.show()
 
-def visualize_monthly_usage(api_url):
+def visualize_monthly_usage(api_url, platform):
+    # Make request to API to retrieve monthly usage data
     # Make request to API to retrieve monthly usage data
     response = requests.get(api_url)
     monthly_usage = response.json()
 
+    # Process monthly usage data
+    if platform == 'ios':
+        # iOS data processing code goes here
+        usage_data = monthly_usage
+    elif platform == 'android':
+        # Android data processing code goes here
+        usage_data = monthly_usage['usage']
+    else:
+        raise ValueError('Invalid platform specified')
+
     # Create bar plot
-    plt.bar(range(len(monthly_usage)), monthly_usage.values(), align='center')
-    plt.xticks(range(len(monthly_usage)), list(monthly_usage.keys()))
+    plt.bar(range(len(usage_data)), usage_data.values(), align='center')
+    plt.xticks(range(len(usage_data)), list(usage_data.keys()))
     plt.xlabel('Month')
     plt.ylabel('Usage (minutes)')
     plt.show()
 
-def visualize_referral_count(api_url):
+def visualize_referral_count(api_url, platform):
     # Make request to API to retrieve referral count data
     response = requests.get(api_url)
     referral_count = response.json()
 
+    # Process referral count data
+    if platform == 'ios':
+        # iOS data processing code goes here
+        referral_data = referral_count
+    elif platform == 'android':
+        # Android data processing code goes here
+        referral_data = referral_count['referrals']
+    else:
+        raise ValueError('Invalid platform specified')
+
     # Create histogram
-    plt.hist(referral_count, bins=10)
+    plt.hist(referral_data, bins=10)
     plt.xlabel('Number of referrals')
     plt.ylabel('Number of users')
     plt.show()
+
